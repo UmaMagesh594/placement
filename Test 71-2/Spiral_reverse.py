bf3bf05923b4fc39f27c1ffcc119b6dbce97379e
Uma Magesh B <152495418+UmaMagesh594@users.noreply.github.com>
2025-02-19 20:29:24 +0530

    Create Spiral_reverse.py

---
+++ Test 71-2/Spiral_reverse.py
@@ -0,0 +1,22 @@
+rows = int(input())
+cols = int(input())
+matrix = [list(map(int, input().split())) for _ in range(rows)]
+top, bottom = 0, len(matrix) - 1
+left, right = 0, len(matrix[0]) - 1
+result = []
+while top <= bottom and left <= right:
+    for i in range(top, bottom + 1):
+        result.append(matrix[i][left])
+    left += 1
+    for i in range(left, right + 1):
+        result.append(matrix[bottom][i])
+    bottom -= 1
+    if left <= right:
+        for i in range(bottom, top - 1, -1):
+            result.append(matrix[i][right])
+        right -= 1
+    if top <= bottom:
+        for i in range(right, left - 1, -1):
+            result.append(matrix[top][i])
+        top += 1
+print(" ".join(map(str, result)))
